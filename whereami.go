c44254430d5734ad949f1ad1df48b999d2646d83
Joseph David Borġ <joe@josephb.org>
2015-11-10 19:30:56 +0000

    Binary to handle errors

--- whereami.go
+++ whereami.go
@@ -5,7 +5,14 @@ import (
 	"github.com/joedborg/whereami/lib"
 )
 
+func errorCheck(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	data := whereami.GetLocationData()
+	data, err := whereami.GetLocationData()
+	errorCheck(err)
 	fmt.Printf("%s, %s\n", data.City, data.Country)
 }
